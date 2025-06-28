dccf4bf3f3d8cc54d119a8c7f0e22e6e4dcbf26e
afk-bro <astro.orbit@proton.me>
2025-06-27 22:55:57 -0700

    feat(login) - Added Login.tsx page, added page to the App for routing. Added translations to the locales files: en, th.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import Home from '@/pages/Home';
+import Register from '@/pages/Register';
+import Login from '@/pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         {/* Future routes: Login, Register, Dashboard, etc. */}
       </Routes>
     </Router>
