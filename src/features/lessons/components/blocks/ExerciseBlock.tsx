b29cd48855c488556b3e755c5e4a5334e3c483b8
afk-bro <astro.orbit@proton.me>
2026-05-02 19:17:29 -0700

    feat(lessons): wire unit-2 exercises into ExerciseBlock

--- src/features/lessons/components/blocks/ExerciseBlock.tsx
+++ src/features/lessons/components/blocks/ExerciseBlock.tsx
@@ -2,6 +2,7 @@ import MultipleChoice from "@/components/exercises/MultipleChoice";
 import FillBlank from "@/components/exercises/FillBlank";
 import type { ExerciseType } from "../../lesson.types";
 import * as unit1Exercises from "../../data/exercises/unit-1";
+import * as unit2Exercises from "../../data/exercises/unit-2";
 import type { McqExercise, FillBlankExercise } from "@/components/exercises/exercises.types";
 
 type Props = {
@@ -25,6 +26,24 @@ const exerciseMap: Record<string, TaggedExercise> = {
   "u1-activities-mcq-4": { type: "multiple-choice", data: unit1Exercises.activitiesWhereMcq },
   "u1-activities-mcq-5": { type: "multiple-choice", data: unit1Exercises.activitiesFirstNameMcq },
   "u1-activities-mcq-6": { type: "multiple-choice", data: unit1Exercises.activitiesLastNameMcq },
+  // unit-2 grammar
+  "u2-grammar-mcq-1": { type: "multiple-choice", data: unit2Exercises.grammarMcqContractions },
+  "u2-grammar-mcq-2": { type: "multiple-choice", data: unit2Exercises.grammarMcqWhereWord },
+  // unit-2 activities: vocabulary recognition
+  "u2-activities-mcq-1": { type: "multiple-choice", data: unit2Exercises.activitiesVocabClassroomMcq },
+  "u2-activities-mcq-2": { type: "multiple-choice", data: unit2Exercises.activitiesVocabHomeMcq },
+  "u2-activities-mcq-3": { type: "multiple-choice", data: unit2Exercises.activitiesVocabTownMcq },
+  "u2-activities-mcq-4": { type: "multiple-choice", data: unit2Exercises.activitiesVocabMixedMcq },
+  // unit-2 activities: 'Where' + pronouns
+  "u2-activities-mcq-5": { type: "multiple-choice", data: unit2Exercises.activitiesWhereResponseMariaMcq },
+  "u2-activities-fb-1": { type: "fill-blank", data: unit2Exercises.activitiesWhereAreFb },
+  "u2-activities-mcq-6": { type: "multiple-choice", data: unit2Exercises.activitiesWhereResponseChildrenMcq },
+  "u2-activities-fb-2": { type: "fill-blank", data: unit2Exercises.activitiesWhereDictionaryFb },
+  // unit-2 activities: contractions
+  "u2-activities-mcq-7": { type: "multiple-choice", data: unit2Exercises.activitiesContractionTheyMcq },
+  "u2-activities-mcq-8": { type: "multiple-choice", data: unit2Exercises.activitiesContractionItMcq },
+  "u2-activities-fb-3": { type: "fill-blank", data: unit2Exercises.activitiesContractionShortenFb },
+  "u2-activities-mcq-9": { type: "multiple-choice", data: unit2Exercises.activitiesContractionCorrectMcq },
 };
 
 export default function ExerciseBlock({ exerciseType, exerciseId, imageUrl, onCorrect }: Props) {
